b1c3a45e22e804d4cfc051c582efd194ac9ebcfe
Ste517 <ste517@users.noreply.github.com>
2022-04-18 20:47:07 +0200

    Test json log 2

--- asta.js
+++ asta.js
@@ -1,5 +1,5 @@
 const json_file = './asta.json';
 
-$.getJSON("test.json", function(json) {
+$.getJSON(json_file, function(json) {
     console.log(json); // this will show the info it in firebug console
 });
