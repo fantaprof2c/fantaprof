9068f367f372f8e20350445a4c3879334a7ec4e6
Ste517 <ste517@users.noreply.github.com>
2022-04-18 20:43:37 +0200

    Test json log

--- asta.js
+++ asta.js
@@ -1,24 +1,5 @@
 const json_file = './asta.json';
 
-async function get_json() {
-    const response = await fetch(json_file);
-    const data = await response.json();
-    return data;
-}
-
-async function post_json() {
-    const response = await fetch(json_file, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            "name": "John",
-            "age": 30
-        })
-    });
-    const data = await response.json();
-    return data;
-}
-
-po
+$.getJSON("test.json", function(json) {
+    console.log(json); // this will show the info it in firebug console
+});
